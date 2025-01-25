00b38da00c50900b7fdf1e8083366b94fea0749d
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-01-24 20:26:02 -0800

    added some assertions for similarity
    not 100% sure on the expectations of filters
    
    not super happy with how the assertions are written

--- packages/langchain-graph-retriever/tests/integration_tests/adapters/test_adapters.py
+++ packages/langchain-graph-retriever/tests/integration_tests/adapters/test_adapters.py
@@ -10,61 +10,99 @@ from tests.animal_docs import load_animal_docs
 from tests.embeddings.simple_embeddings import AnimalEmbeddings
 from tests.integration_tests.stores import StoreFactory
 
+def assert_valid_result(doc: Document):
+    assert isinstance(doc.id, str)
 
-def assert_ids_any_order(results: Iterable[Document], expected: list[str]) -> None:
-    assert sorted([r.id for r in results]) == expected
+    assert METADATA_EMBEDDING_KEY in doc.metadata
+    assert_is_embedding(doc.metadata[METADATA_EMBEDDING_KEY])
 
-def assert_valid_results(results: Iterable[Document]) -> None:
-    for doc in results:
-        assert doc.id is not None, "documents must have IDs"
+def assert_is_embedding(value):
+    assert isinstance(value, list)
+    for item in value:
+        assert isinstance(item, float)
 
-        embedding = doc.metadata.get(METADATA_EMBEDDING_KEY)
-        assert isinstance(embedding, list) and all(
-            [isinstance(n, float) for n in embedding]
-        ), f"metadata[METADATA_EMBEDDING_KEY] must be a list[float], but was {embedding}"
+def assert_valid_results(docs: list[Document]):
+    for doc in docs:
+        assert_valid_result(doc)
 
+def assert_ids_any_order(results: Iterable[Document],
+                         expected: list[str]) -> None:
+    assert_valid_results(results)
+
+    result_ids = [r.id for r in results]
+    assert len(set(result_ids)) == len(result_ids), "should not contain duplicates"
+    assert set(result_ids) == set(expected), "should contain exactly expected IDs"
 
 class AdapterComplianceSuite:
     def test_get_one(self, adapter: Adapter) -> None:
         results = adapter.get(["boar"])
         assert_ids_any_order(results, ["boar"])
-        assert_valid_results(results)
 
     async def test_aget_one(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar"])
         assert_ids_any_order(results, ["boar"])
-        assert_valid_results(results)
 
     def test_get_many(self, adapter: Adapter) -> None:
         results = adapter.get(["boar", "chinchilla", "cobra"])
         assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
-        assert_valid_results(results)
 
     async def test_aget_many(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar", "chinchilla", "cobra"])
         assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
-        assert_valid_results(results)
 
     def test_get_missing(self, adapter: Adapter) -> None:
         results = adapter.get(["boar", "chinchilla", "unicorn", "cobra"])
         assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
-        assert_valid_results(results)
 
     async def test_aget_missing(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar", "chinchilla", "unicorn", "cobra"])
         assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
-        assert_valid_results(results)
 
     def test_get_duplicate(self, adapter: Adapter) -> None:
         results = adapter.get(["boar", "chinchilla", "boar", "cobra"])
         assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
-        assert_valid_results(results)
 
     async def test_aget_duplicate(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar", "chinchilla", "boar", "cobra"])
         assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
-        assert_valid_results(results)
 
+    def test_similarity_search_with_embedding(self, adapter: Adapter) -> None:
+        embedding, results = adapter.similarity_search_with_embedding("domesticated hunters")
+        assert_is_embedding(embedding)
+        assert_ids_any_order(results, ["cat", "horse", "chicken", "llama"])
+
+    def test_similarity_search_with_embedding_respects_k(
+        self, adapter: Adapter
+    ) -> None:
+        embedding, results = adapter.similarity_search_with_embedding(
+            "domesticated hunters", k=2
+        )
+        assert_is_embedding(embedding)
+        assert_ids_any_order(results, ["cat", "horse"])
+
+    def test_similarity_search_with_embedding_respects_value_filters(
+        self, adapter: Adapter
+    ) -> None:
+        embedding, results = adapter.similarity_search_with_embedding(
+            "domesticated hunters", filter={"type": "mammal"}
+        )
+        assert_is_embedding(embedding)
+        assert_ids_any_order(results, ["cat", "dog", "horse", "llama"])
+
+    def test_similarity_search_with_embedding_respects_list_filters(
+        self, adapter: Adapter
+    ) -> None:
+        embedding, results = adapter.similarity_search_with_embedding(
+            "domesticated hunters", filter={"keywords": "hunting"}
+        )
+        assert_is_embedding(embedding)
+        assert_ids_any_order(results, ["cat", "dog", "horse", "llama"])
+
+        embedding, results = adapter.similarity_search_with_embedding(
+            "domesticated hunters", filter={"keywords": ["hunting", "agile"]}
+        )
+        assert_is_embedding(embedding)
+        assert_ids_any_order(results, ["cat", "dog", "horse", "llama"])
 
 class TestBuiltinAdapters(AdapterComplianceSuite):
     @pytest.fixture(scope="class")
