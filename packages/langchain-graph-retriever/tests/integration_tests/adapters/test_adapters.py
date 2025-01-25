4cbfcd6aa392470be79fb5b36a8a84152b35ad14
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-01-24 16:47:20 -0800

    update tests

--- packages/langchain-graph-retriever/tests/integration_tests/adapters/test_adapters.py
+++ packages/langchain-graph-retriever/tests/integration_tests/adapters/test_adapters.py
@@ -1,74 +1,82 @@
 import abc
-from typing import Iterable
+from collections.abc import Iterable
+
+import pytest
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
-import pytest
+from langchain_graph_retriever.adapters.base import METADATA_EMBEDDING_KEY, Adapter
 
-from langchain_graph_retriever.adapters.base import Adapter, METADATA_EMBEDDING_KEY
-from tests.integration_tests.stores import StoreFactory
-from tests.embeddings.simple_embeddings import AnimalEmbeddings
 from tests.animal_docs import load_animal_docs
+from tests.embeddings.simple_embeddings import AnimalEmbeddings
+from tests.integration_tests.stores import StoreFactory
+
+
+def assert_ids_any_order(results: Iterable[Document], expected: list[str]) -> None:
+    assert sorted([r.id for r in results]) == expected
 
 def assert_valid_results(results: Iterable[Document]) -> None:
     for doc in results:
         assert doc.id is not None, "documents must have IDs"
 
         embedding = doc.metadata.get(METADATA_EMBEDDING_KEY)
-        assert isinstance(embedding, list) and all([isinstance(n, float) for n in embedding]), (
-            "metadata[METADATA_EMBEDDING_KEY] must be a list[float]"
-        )
+        assert isinstance(embedding, list) and all(
+            [isinstance(n, float) for n in embedding]
+        ), f"metadata[METADATA_EMBEDDING_KEY] must be a list[float], but was {embedding}"
+
 
 class AdapterComplianceSuite:
     def test_get_one(self, adapter: Adapter) -> None:
         results = adapter.get(["boar"])
-        assert {r.id for r in results} == {"boar"}
+        assert_ids_any_order(results, ["boar"])
         assert_valid_results(results)
 
     async def test_aget_one(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar"])
-        assert {r.id for r in results} == {"boar"}
+        assert_ids_any_order(results, ["boar"])
         assert_valid_results(results)
 
     def test_get_many(self, adapter: Adapter) -> None:
         results = adapter.get(["boar", "chinchilla", "cobra"])
-        assert {r.id for r in results} == {"boar", "chinchilla", "cobra"}
+        assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
         assert_valid_results(results)
 
     async def test_aget_many(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar", "chinchilla", "cobra"])
-        assert {r.id for r in results} == {"boar", "chinchilla", "cobra"}
+        assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
         assert_valid_results(results)
 
     def test_get_missing(self, adapter: Adapter) -> None:
         results = adapter.get(["boar", "chinchilla", "unicorn", "cobra"])
-        assert {r.id for r in results} == {"boar", "chinchilla", "cobra"}
+        assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
         assert_valid_results(results)
 
     async def test_aget_missing(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar", "chinchilla", "unicorn", "cobra"])
-        assert {r.id for r in results} == {"boar", "chinchilla", "cobra"}
+        assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
         assert_valid_results(results)
 
     def test_get_duplicate(self, adapter: Adapter) -> None:
         results = adapter.get(["boar", "chinchilla", "boar", "cobra"])
-        assert {r.id for r in results} == {"boar", "chinchilla", "cobra"}
+        assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
         assert_valid_results(results)
 
     async def test_aget_duplicate(self, adapter: Adapter) -> None:
         results = await adapter.aget(["boar", "chinchilla", "boar", "cobra"])
-        assert {r.id for r in results} == {"boar", "chinchilla", "cobra"}
+        assert_ids_any_order(results, ["boar", "chinchilla", "cobra"])
         assert_valid_results(results)
 
 
 class TestBuiltinAdapters(AdapterComplianceSuite):
     @pytest.fixture(scope="class")
-    def adapter(self,
-                store_factory: StoreFactory,
-                request: pytest.FixtureRequest) -> Adapter:
-        return store_factory.create(request,
-                                    embedding=AnimalEmbeddings(),
-                                    docs=load_animal_docs(),
+    def adapter(
+        self, store_factory: StoreFactory, request: pytest.FixtureRequest
+    ) -> Adapter:
+        return store_factory.create(
+            request,
+            embedding=AnimalEmbeddings(),
+            docs=load_animal_docs(),
         )
+
 
 class TestAdapterCompliance(abc.ABC, AdapterComplianceSuite):
     """
