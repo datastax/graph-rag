3e45f18b690e58b184cac637b2072beabaf90738
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-02-03 10:49:52 -0800

    make it a function

--- packages/graph-rag-example-helpers/src/graph_rag_example_helpers/datasets/animals.py
+++ packages/graph-rag-example-helpers/src/graph_rag_example_helpers/datasets/animals.py
@@ -6,22 +6,29 @@ from langchain_core.documents import Document
 ANIMALS_JSONL_URL = "https://raw.githubusercontent.com/datastax/graph-rag/refs/heads/main/data/animals.jsonl"
 
 
-class Animals:
-    """Download a list of Documents for experimenting with Graph-Retriever."""
+def fetch_animal_documents() -> list[Document]:
+    """Download and parse a list of Documents for use with Graph Retriever.
 
-    def __init__(self):
-        response = requests.get(ANIMALS_JSONL_URL)
-        response.raise_for_status()  # Ensure we got a valid response
+    This is a small example dataset with useful links.
 
-        self.documents = []
-        for line in response.text.splitlines():
-            data = json.loads(line)  # Parse each line as JSON
-            self.documents.append(
-                Document(
-                    id=data["id"], page_content=data["text"], metadata=data["metadata"]
-                )
-            )
+    This method downloads the dataset each time -- generally it is preferable
+    to invoke this only once and store the documents in memory or a vector
+    store.
 
-    def docs(self) -> list[Document]:
-        """Get a list of Documents for experimenting with Graph-Retriever."""
-        return self.documents
+    Returns
+    -------
+    list[Document]
+        The fetched animal documetns.
+    """
+    response = requests.get(ANIMALS_JSONL_URL)
+    response.raise_for_status()  # Ensure we got a valid response
+
+    return [
+        Document(
+            id=data["id"],
+            page_content=data["text"],
+            metadata=data["metadata"]
+        )
+        for line in response.text.splitlines()
+        if (data := json.loads(line))
+    ]
